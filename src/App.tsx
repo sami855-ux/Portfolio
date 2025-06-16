9a5cae84db2266e7c1819ae2e670c6076e63f533
sami855-ux <samitale56@outlook.com>
2025-06-16 17:15:31 +0300

    add Collab component to App for improved navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header"
 import { About } from "./pages/About"
+import Collab from "./pages/collab"
 import { Footer } from "./pages/Footer"
 import Home from "./pages/Home"
 import Projects from "./pages/Projects"
@@ -13,6 +14,7 @@ const App = () => {
       <Skills />
       <About />
       <Projects />
+      <Collab />
       <Footer />
     </>
   )
